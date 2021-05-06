5156538a5a68b926f4dbe558af928e3bfba65a70
Elli K <kauhanen.elli@gmail.com>
2021-05-06 20:04:15 +0300

    Revert "selvitän sotkua"
    This reverts commit 1201d84c1b31df0a35fa9687d7200a9b0eca1a14.

--- Mokkivarausjarjestelma2.0/Program.cs
+++ Mokkivarausjarjestelma2.0/Program.cs
@@ -16,7 +16,7 @@ namespace Mokkivarausjarjestelma2._0
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new toiminta_alue());
+            Application.Run(new palveluhallinta());
         }
     }
 }
