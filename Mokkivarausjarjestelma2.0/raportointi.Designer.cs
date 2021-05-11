e857783f466ddfebdd1a3541f55c0dc748c51d63
Calig <santeri_kasurinen@hotmail.com>
2021-05-11 10:58:31 +0300

    Kaikki toiminnot lisätty

--- Mokkivarausjarjestelma2.0/raportointi.Designer.cs
+++ Mokkivarausjarjestelma2.0/raportointi.Designer.cs
@@ -28,70 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cbToimialueet = new System.Windows.Forms.ComboBox();
+            this.btnLataa = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.rbMajoitukset = new System.Windows.Forms.RadioButton();
+            this.rbPalvelut = new System.Windows.Forms.RadioButton();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(320, 77);
+            this.dateTimePicker1.CustomFormat = "MMMM yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(17, 55);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
             this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 5, 7, 15, 13, 33, 0);
+            // 
+            // cbToimialueet
+            // 
+            this.cbToimialueet.FormattingEnabled = true;
+            this.cbToimialueet.Location = new System.Drawing.Point(17, 28);
+            this.cbToimialueet.Name = "cbToimialueet";
+            this.cbToimialueet.Size = new System.Drawing.Size(121, 21);
+            this.cbToimialueet.TabIndex = 4;
+            // 
+            // btnLataa
+            // 
+            this.btnLataa.Location = new System.Drawing.Point(244, 28);
+            this.btnLataa.Name = "btnLataa";
+            this.btnLataa.Size = new System.Drawing.Size(121, 47);
+            this.btnLataa.TabIndex = 7;
+            this.btnLataa.Text = "Lataa";
+            this.btnLataa.UseVisualStyleBackColor = true;
+            this.btnLataa.Click += new System.EventHandler(this.btnLataa_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(184, 113);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 94);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(437, 302);
-            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(464, 287);
+            this.dataGridView1.TabIndex = 9;
             // 
-            // checkBox1
+            // statusStrip1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(71, 136);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 392);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(964, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
             // 
-            // checkBox2
+            // toolStripStatusLabel1
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(71, 113);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
             // 
-            // comboBox1
+            // rbMajoitukset
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(184, 77);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 4;
+            this.rbMajoitukset.AutoSize = true;
+            this.rbMajoitukset.Checked = true;
+            this.rbMajoitukset.Location = new System.Drawing.Point(144, 32);
+            this.rbMajoitukset.Name = "rbMajoitukset";
+            this.rbMajoitukset.Size = new System.Drawing.Size(79, 17);
+            this.rbMajoitukset.TabIndex = 11;
+            this.rbMajoitukset.TabStop = true;
+            this.rbMajoitukset.Text = "Majoitukset";
+            this.rbMajoitukset.UseVisualStyleBackColor = true;
+            // 
+            // rbPalvelut
+            // 
+            this.rbPalvelut.AutoSize = true;
+            this.rbPalvelut.Location = new System.Drawing.Point(144, 55);
+            this.rbPalvelut.Name = "rbPalvelut";
+            this.rbPalvelut.Size = new System.Drawing.Size(63, 17);
+            this.rbPalvelut.TabIndex = 12;
+            this.rbPalvelut.Text = "Palvelut";
+            this.rbPalvelut.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(487, 94);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(464, 287);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
             // 
             // raportointi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(964, 414);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.rbPalvelut);
+            this.Controls.Add(this.rbMajoitukset);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLataa);
+            this.Controls.Add(this.cbToimialueet);
             this.Controls.Add(this.dateTimePicker1);
             this.Name = "raportointi";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.raportointi_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,9 +195,13 @@
         #endregion
 
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cbToimialueet;
+        private System.Windows.Forms.Button btnLataa;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.RadioButton rbMajoitukset;
+        private System.Windows.Forms.RadioButton rbPalvelut;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- Mokkivarausjarjestelma2.0/raportointi.cs
+++ Mokkivarausjarjestelma2.0/raportointi.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace Mokkivarausjarjestelma2._0
 {
@@ -15,6 +20,116 @@ namespace Mokkivarausjarjestelma2._0
         public raportointi()
         {
             InitializeComponent();
+            this.dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void raportointi_Load(object sender, EventArgs e)
+        {
+            populateDGV();
+            populateComboboxes();
+        }
+
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3308;Initial Catalog='vn';username=root;password=asdlol");
+
+        public void populateDGV()
+        {
+            string majoitus_query = "SELECT varaus_id AS 'ID', varattu_alkupvm AS Varattu, nimi AS 'Toiminta-alue',asiakas.sukunimi,asiakas.etunimi FROM varaus INNER JOIN mokki ON varaus.mokki_mokki_id = mokki.mokki_id INNER JOIN toimintaalue ON toimintaalue.toimintaalue_id = mokki.toimintaalue_id INNER JOIN asiakas ON asiakas.asiakas_id = varaus.asiakas_id WHERE toimintaalue.nimi = '" + cbToimialueet.Text + "' AND MONTH(varattu_alkupvm) = '" + dateTimePicker1.Value.Month + "' AND YEAR(varattu_alkupvm) = '" + dateTimePicker1.Value.Year + "' GROUP BY varaus.varaus_id";
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(majoitus_query, connection);
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        public void populateComboboxes()
+        {
+
+            string queryToiminta = "SELECT * FROM toimintaalue";
+
+            MySqlDataAdapter adapter2 = new MySqlDataAdapter(queryToiminta, connection);
+            DataSet dsCombobox = new DataSet();
+            adapter2.Fill(dsCombobox, "toimintaalue");
+            cbToimialueet.DisplayMember = "nimi";
+            cbToimialueet.DataSource = dsCombobox.Tables["toimintaalue"];
+        }
+
+        private void btnLataa_Click(object sender, EventArgs e)
+        {
+
+            if (!rbMajoitukset.Checked)
+            {
+                string palvelu_query = "SELECT varauksen_palvelut.palvelu_id AS ID, toimintaalue.nimi AS 'Toiminta-alue',palvelu.nimi,SUM(lkm) AS Asiakkaita,SUM(lkm * palvelu.hinta) AS 'Yhteensa' FROM varauksen_palvelut INNER JOIN varaus ON varauksen_palvelut.varaus_id = varaus.varaus_id INNER JOIN palvelu ON varauksen_palvelut.palvelu_id = palvelu.palvelu_id INNER JOIN toimintaalue ON toimintaalue.toimintaalue_id = palvelu.toimintaalue_id INNER JOIN asiakas ON asiakas.asiakas_id = varaus.asiakas_id WHERE toimintaalue.nimi = '" + cbToimialueet.Text + "' AND MONTH(varattu_alkupvm) = '" + dateTimePicker1.Value.Month + "' AND YEAR(varattu_alkupvm) = '" + dateTimePicker1.Value.Year + "' GROUP BY varauksen_palvelut.palvelu_id";
+                DataTable table = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(palvelu_query, connection);
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+                chart1.Series.Clear();
+
+                Series S = chart1.Series.Add(dataGridView1.Columns[3].HeaderText);
+                S.ChartType = SeriesChartType.Column;
+                S.IsValueShownAsLabel = true;
+
+                Series A = chart1.Series.Add(dataGridView1.Columns[4].HeaderText);
+                A.ChartType = SeriesChartType.Column;
+                A.IsValueShownAsLabel = true;
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+
+                    //chartBpComplaince.Series.Clear();
+                    A.Points.AddXY(row.Cells[2].Value.ToString(), row.Cells[4].Value.ToString());
+                    A.ChartType = SeriesChartType.Column;
+                    A.IsValueShownAsLabel = true;
+                    S.Points.AddXY(row.Cells[2].Value.ToString(), row.Cells[3].Value.ToString());
+                    S.ChartType = SeriesChartType.Column;
+                    S.IsValueShownAsLabel = true;
+                }
+            }
+
+            else
+            {
+                string majoitus_query = "SELECT mokki_mokki_id AS ID, nimi AS 'Toiminta-alue',mokki.mokkinimi,COUNT(mokki.mokki_id) AS varauksia,SUM(mokki.mokki_id * mokki.hinta) AS tuotto FROM varaus INNER JOIN mokki ON varaus.mokki_mokki_id = mokki.mokki_id INNER JOIN toimintaalue ON toimintaalue.toimintaalue_id = mokki.toimintaalue_id INNER JOIN asiakas ON asiakas.asiakas_id = varaus.asiakas_id WHERE toimintaalue.nimi = '" + cbToimialueet.Text + "' AND MONTH(varattu_alkupvm) = '" + dateTimePicker1.Value.Month + "' AND YEAR(varattu_alkupvm) = '" + dateTimePicker1.Value.Year + "' GROUP BY varaus.mokki_mokki_id";
+                DataTable table = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(majoitus_query, connection);
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+                chart1.Series.Clear();
+
+                Series S = chart1.Series.Add(dataGridView1.Columns[3].HeaderText);
+                S.ChartType = SeriesChartType.Column;
+                S.IsValueShownAsLabel = true;
+
+                Series A = chart1.Series.Add(dataGridView1.Columns[4].HeaderText);
+                A.ChartType = SeriesChartType.Column;
+                A.IsValueShownAsLabel = true;
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+
+                    //chartBpComplaince.Series.Clear();
+                    A.Points.AddXY(row.Cells[2].Value.ToString(), row.Cells[4].Value.ToString());
+                    A.ChartType = SeriesChartType.Column;
+                    A.IsValueShownAsLabel = true;
+                    S.Points.AddXY(row.Cells[2].Value.ToString(), row.Cells[3].Value.ToString());
+                    S.ChartType = SeriesChartType.Column;
+                    S.IsValueShownAsLabel = true;
+                }
+
+            }
+
+            if (dataGridView1.Rows.Count == 0)
+            {
+                if (!rbMajoitukset.Checked)
+                    toolStripStatusLabel1.Text = "Virhe: Palveluita ei löytynyt valitulla ajankohdalla.";
+                else
+                    toolStripStatusLabel1.Text = "Virhe: Majoituksia ei löytynyt valitulla ajankohdalla.";
+            }
+            else if (dataGridView1.Rows.Count > 0)
+            {
+                if (!rbMajoitukset.Checked)
+                    toolStripStatusLabel1.Text = "Varattuja palveluita löydettiin " + dataGridView1.Rows.Count + " kpl";
+                else
+                    toolStripStatusLabel1.Text = "Varattuja majoituksia löydettiin " + dataGridView1.Rows.Count + " kpl";
+            }
         }
     }
 }
